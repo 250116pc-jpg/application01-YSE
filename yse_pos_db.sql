ec53656acfc9b7ec4c8335052b4c8a49910e02e1
250116pc-jpg <250116pc@yse-c.net>
2026-04-23 14:35:02 +0900

    Add files via upload

--- yse_pos_db.sql
+++ yse_pos_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: 127.0.0.1
--- 生成日時: 2026-04-23 10:14:59
+-- 生成日時: 2026-04-23 14:34:11
 -- サーバのバージョン： 10.4.32-MariaDB
 -- PHP のバージョン: 8.2.12
 
@@ -24,20 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- テーブルの構造 `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(50) NOT NULL COMMENT 'ログインID',
-  `password_hash` varchar(255) NOT NULL COMMENT 'パスワードハッシュ',
-  `role` tinyint(1) NOT NULL DEFAULT 0 COMMENT '権限（0:ユーザ、1:管理者）',
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT '登録日時',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_id_unique` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
 -- テーブルの構造 `sales`
 --
 
@@ -54,7 +40,30 @@ CREATE TABLE `sales` (
 
 INSERT INTO `sales` (`id`, `customer_id`, `amount`, `created_at`) VALUES
 (1, 1, 66, '2026-04-23 01:00:20'),
-(2, 1, 55, '2026-04-23 01:14:18');
+(2, 1, 55, '2026-04-23 01:14:18'),
+(3, 1, 4446222, '2026-04-23 05:17:26'),
+(4, 1, 32, '2026-04-23 05:21:50');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(50) NOT NULL COMMENT 'ログインID',
+  `password_hash` varchar(255) NOT NULL COMMENT 'パスワードハッシュ',
+  `role` tinyint(1) NOT NULL DEFAULT 0 COMMENT '権限（0:ユーザ、1:管理者）',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT '登録日時'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- テーブルのデータのダンプ `users`
+--
+
+INSERT INTO `users` (`id`, `user_id`, `password_hash`, `role`, `created_at`) VALUES
+(1, '1234', '$2y$10$id/Be/U5tlqWQJ3AO7n3SO5tVOZOTgNjxNYewdBbiZcXsIxVFIzam', 0, '2026-04-23 05:16:55');
 
 --
 -- ダンプしたテーブルのインデックス
@@ -67,6 +76,13 @@ ALTER TABLE `sales`
   ADD PRIMARY KEY (`id`);
 
 --
+-- テーブルのインデックス `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_id_unique` (`user_id`);
+
+--
 -- ダンプしたテーブルの AUTO_INCREMENT
 --
 
@@ -74,7 +90,13 @@ ALTER TABLE `sales`
 -- テーブルの AUTO_INCREMENT `sales`
 --
 ALTER TABLE `sales`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '売上ID', AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '売上ID', AUTO_INCREMENT=5;
+
+--
+-- テーブルの AUTO_INCREMENT `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
