37a7b560782ab89bbf742a8fb493dcd148976043
250056wi-cpu <250056wi@yse-c.net>
2026-05-28 11:14:51 +0900

    Update index.php

--- index.php
+++ index.php
@@ -49,6 +49,8 @@ $displayItemCount = cartCount($receiptRows);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>YSEレジ</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="../favicon.jpg">
+
     
     <style>
         @media print {
